daad097aaaa4bc2d499836a5ecf07ae45e840ceb
jburress <johnw.burress@yahoo.com>
2020-01-05 21:24:31 -0800

    Update CameraMovement.cs

--- CameraMovement.cs
+++ CameraMovement.cs
@@ -7,14 +7,27 @@ public class CameraMovement : MonoBehaviour
     public float speed = 8;
     public float doubleSpeed = 16;
     public int creatable;
+    public Camera MainCamera, Camera2;//You must create Camera2 in your Unity project and position it to your liking. 
 
     void Start()
     {
         creatable = 0;
+        MainCamera.gameObject.SetActive(true);
+        Camera2.gameObject.SetActive(false);
     }
 
     void Update()
     {
+        if (Input.GetKey(KeyCode.L) && (Input.GetKey(KeyCode.LeftControl)))//Left CTRL + L to create Crate
+        {
+            CreateCrate();
+            StartCoroutine(waiter());
+        }
+        if (Input.GetKey(KeyCode.C) && (Input.GetKey(KeyCode.LeftControl)))//Left CTRL + C to create Cylinder
+        {
+            CreateCylinder();
+            StartCoroutine(waiter());
+        }
         if (Input.GetKey(KeyCode.N) && (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl)))
         {
             creatable++;
@@ -56,6 +69,16 @@ public class CameraMovement : MonoBehaviour
             CreateObject();
             StartCoroutine(Waiter());
         }
+          if(Input.GetKey(KeyCode.Z) && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))//switch cameras to MainCamera
+        {
+                MainCamera.gameObject.SetActive(true);
+                Camera2.gameObject.SetActive(false);
+        }
+        if (Input.GetKey(KeyCode.X) && (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)))//switch cameras to Camera2
+        {
+            MainCamera.gameObject.SetActive(false);
+            Camera2.gameObject.SetActive(true);
+        }
     }
     public void CreateObject()
     {
@@ -73,5 +96,25 @@ public class CameraMovement : MonoBehaviour
     {
         yield return new WaitForSeconds(5);
         creatable = 0;
+    }
+    public void CreateCylinder()//create standard Cylinder
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.AddComponent(typeof(Rigidbody));
+        cube.AddComponent(typeof(Movement2));
+        cube.GetComponent<Rigidbody>().isKinematic = false;
+        var cubeRenderer = cube.GetComponent<Renderer>();
+        cubeRenderer.material.SetColor("_Color", Color.black);
+        cube.transform.localScale = new Vector3(1f, 1f, 1f);
+        cube.transform.position = new Vector3(0, 5f, 0);
+    }
+
+    public void CreateCrate()//create standard Crate
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.AddComponent(typeof(Rigidbody));
+        cube.AddComponent(typeof(Movement2));
+        cube.transform.localScale = new Vector3(2f, 2f, 2f);
+        cube.transform.position = new Vector3(0, 0.5f, 0);
     }
 }
